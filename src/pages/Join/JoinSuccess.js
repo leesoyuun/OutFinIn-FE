a65d4b315b82a36d41767387413bf164b570301e
leesoyuun <leesoyun4206@naver.com>
2023-09-15 16:06:32 +0900

    Add 3dhanger img of joinsuccess page

--- src/pages/Join/JoinSuccess.js
+++ src/pages/Join/JoinSuccess.js
@@ -5,7 +5,11 @@ import * as f from '../../components/Common/CommonStyle';
 import ButtonBottom from '../../components/Common/ButtonBottom';
 import ButtonNumbers from '../../components/Join/NumbersButton';
 import QuestionMode from '../../components/Join/QuestionModeBox';
+import Hanger3D from '../../assets/img/3Dhanger.svg';
 
+const Hanger3DImg = styled.img`
+    margin: 0 auto;
+`;
 
 const JoinSuccess = () => {
     
@@ -23,10 +27,11 @@ const JoinSuccess = () => {
                         <QuestionMode content={localStorage.getItem('mode') == 1 ? 
                         '회원가입이 완료 되었습니다!\n 오늘도 예쁜 코디 부탁드려요':
                         '회원가입이 완료 되었습니다! \n오늘도 딱 맞는 코디 받으세요'} marginBottom={'2.96vh'}/>
+                        <Hanger3DImg src={Hanger3D}></Hanger3DImg> 
                         <Link to={localStorage.getItem('mode') == 1 ? 
                         "../outermainpage":
                         "../usermainpage"} style={{ textDecoration: 'none' }}>
-                            <ButtonBottom content={'다음'} />
+                            <ButtonBottom content={'시작하기'} />
                         </Link>
                     </f.ScreenJoin>
                 </f.ScreenComponent>
